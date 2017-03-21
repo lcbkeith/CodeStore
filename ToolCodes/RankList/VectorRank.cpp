27e5b9bb58366a444c15c95a81441dfc69a4e116
lichaobin <591699198@qq.com>
2017-03-21 19:52:42 +0800

    不保存反向映射的ranklist

---
+++ ToolCodes/RankList/VectorRank.cpp
@@ -0,0 +1,171 @@
+#include "VectorRank.h"
+#include <algorithm>
+
+RankList::RankList()
+	:m_rankType(RANK_COVER),
+	m_autoSort(true),
+	m_maxSize(0),
+	m_inited(false)
+{
+
+}
+
+RankList::RankList(RankType type, bool autoSort /*= true*/, int maxSize /*= 0*/)
+	:m_rankType(type),
+	m_autoSort(autoSort),
+	m_maxSize(maxSize),
+	m_inited(true)
+{
+	if (m_maxSize > 0)
+	{
+		m_rankVec.reserve(m_maxSize);
+	}
+}
+
+void RankList::Init(RankType type, bool autoSort /*= true*/, int maxSize /*= 0*/)
+{
+	m_rankType = type;
+	m_autoSort = autoSort;
+	m_maxSize = maxSize;
+	Clear();
+	m_sorted = true;
+	m_inited = true;
+}
+
+const RankVec& RankList::GetRankVec() const
+{
+	return m_rankVec;
+}
+
+void RankList::Sort()
+{
+	if (m_sorted)
+	{
+		return;
+	}
+	sort(m_rankVec.begin(), m_rankVec.end(), greaterRankVal());
+	m_sorted = true;
+}
+
+
+RankItem* RankList::GetIndex(int index)
+{
+	if (index < 0 || index >= (int)m_rankVec.size())
+	{
+		return NULL;
+	}
+	return &m_rankVec[index];
+}
+
+int RankList::LookupRankById(int64 id, const RankItem*& outItem)
+{
+	RankVec::iterator iter = std::find_if(m_rankVec.begin(), m_rankVec.end(), vectorFinder<RankItem>(id));
+	if (iter == m_rankVec.end())
+	{
+		outItem = NULL;
+		return -1;
+	}
+	outItem = &(*iter);
+	return iter - m_rankVec.begin();
+}
+
+void RankList::Reserve(size_t size)
+{
+	m_rankVec.reserve(size);
+}
+
+bool RankList::IsFull()
+{
+	return m_maxSize > 0 && m_rankVec.size() >= m_maxSize;
+}
+
+bool RankList::PushRankItem(RankItem& rankItem)
+{
+	if (m_rankVec.empty())
+	{
+		m_rankVec.push_back(rankItem);
+		return true;
+	}
+	RankVec::iterator vecIter = std::find_if(m_rankVec.begin(), m_rankVec.end(), vectorFinder<RankItem>(rankItem.m_id));
+	if (vecIter != m_rankVec.end())
+	{
+		int64 nowValue = vecIter->m_rankVal;
+		//if (m_rankType == RANK_COVER)
+		//{
+		// 			
+		//}
+		if (m_rankType == RANK_TAKEBIGGER)
+		{
+			if (nowValue >= rankItem.m_rankVal)
+			{
+				return false;
+			}
+		}
+		else if (m_rankType == RANK_ACCUMULATION)
+		{
+			rankItem.m_rankVal = nowValue + rankItem.m_rankVal;
+		}
+
+		if (rankItem.m_rankVal == nowValue)
+		{
+			return false;
+		}
+
+		if (m_autoSort)
+		{
+			m_rankVec.erase(vecIter);
+			m_rankVec.insert(std::upper_bound(m_rankVec.begin(), m_rankVec.end(), rankItem, greaterRankVal()), rankItem);
+		}
+		else
+		{
+			vecIter->m_rankVal = rankItem.m_rankVal;
+		}
+	}
+	else
+	{
+		if (IsFull())
+		{
+			Sort();
+			if (rankItem.m_rankVal <= m_rankVec.back().m_rankVal)
+			{
+				return false;
+			}
+			m_rankVec.pop_back();
+			m_rankVec.insert(std::upper_bound(m_rankVec.begin(), m_rankVec.end(), rankItem, greaterRankVal()), rankItem);
+			return true;
+		}
+		if (m_autoSort)
+		{
+			m_rankVec.insert(std::upper_bound(m_rankVec.begin(), m_rankVec.end(), rankItem, greaterRankVal()), rankItem);
+		}
+		else
+		{
+			m_rankVec.push_back(rankItem);
+			m_sorted = false;
+		}
+	}
+	return true;
+}
+
+bool RankList::DeleteRankItem(int64 id)
+{
+	RankVec::iterator iter = std::find_if(m_rankVec.begin(), m_rankVec.end(), vectorFinder<RankItem>(id));
+	if (iter != m_rankVec.end())
+	{
+		m_rankVec.erase(iter);
+		return true;
+	}
+	return false;
+}
+
+void RankList::Clear()
+{
+	m_rankVec.clear();
+	m_sorted = true;
+}
+
+bool RankList::Inited()
+{
+	return m_inited;
+}
+
---
+++ ToolCodes/RankList/VectorRank.h
@@ -0,0 +1,91 @@
+#pragma once
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+typedef __int64 int64;
+class RankItem
+{
+public:
+	RankItem() :m_id(-1), m_rankVal(0), m_level(0), m_camp(0) {}
+	RankItem(int64 oid, int64 rankVal) :
+		m_id(oid),
+		m_rankVal(rankVal),
+		m_level(0),
+		m_camp(0) {}
+
+	int64	m_id;
+	string	m_head;
+	string	m_name;
+	int64	m_rankVal;
+	int		m_level;
+	int		m_camp;
+};
+
+template<class T>
+struct vectorFinder
+{
+public:
+	vectorFinder(const int64 id) :m_id(id) {}
+	bool operator()(T const& rank) {
+		return rank.m_id == m_id;
+	}
+private:
+	int64 m_id;
+};
+
+struct greaterRankVal
+{
+	bool operator()(RankItem const& item1, RankItem const& item2)
+	{
+		if (item1.m_rankVal > item2.m_rankVal)
+			return true;
+		else
+			return false;
+	}
+};
+
+typedef std::vector<RankItem> RankVec;
+typedef std::map<int64, int> RankIndexMap;
+
+enum RankType
+{
+	RANK_COVER = 1,			//无脑覆盖原值
+	RANK_TAKEBIGGER = 2,	//取大值
+	RANK_ACCUMULATION = 3,	//累加
+};
+class RankList
+{
+public:
+	RankList();
+	RankList(RankType type, bool autoSort = true, int maxSize = 0);
+	void Init(RankType type, bool autoSort = true, int maxSize = 0);
+
+	const RankVec& GetRankVec() const;
+	void Sort();									//手动排序
+	int GetIndexById(int64 id);						//获取id对应的排序index
+	RankItem* GetIndex(int index);
+	int LookupRankById(int64 id, const RankItem*& outItem);
+	void Reserve(size_t size);
+
+	bool PushRankItem(RankItem& rankItem);
+	bool DeleteRankItem(int64 id);
+	void Clear();
+private:
+	bool IsFull();
+	bool Inited();
+
+	RankVec m_rankVec;
+	int m_maxSize;
+	bool m_autoSort;	//是否自动排序
+	RankType m_rankType;
+
+	bool m_sorted;
+	bool m_inited;
+};
+
+
+
+
